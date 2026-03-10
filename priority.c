223689ec068b9a9000295ec5d82857ec407357f7
Midhuna K <midhunak633@gmail.com>
2026-03-11 02:51:32 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
-struct process{
+struct process
+{
     char pid[10];
     int at, bt, pr;
     int ct, wt, tat;
@@ -14,7 +14,6 @@ int main()
     float avg_wt=0,avg_tat=0;
 
     struct process p[20];
-    struct process order[20];
 
     scanf("%d",&n);
 
@@ -43,11 +42,10 @@ int main()
         if(idx!=-1)
         {
             time+=p[idx].bt;
+
             p[idx].ct=time;
             p[idx].tat=p[idx].ct-p[idx].at;
             p[idx].wt=p[idx].tat-p[idx].bt;
-
-            order[completed]=p[idx];
 
             avg_wt+=p[idx].wt;
             avg_tat+=p[idx].tat;
@@ -63,11 +61,11 @@ int main()
 
     printf("Waiting Time:");
     for(i=0;i<n;i++)
-        printf(" %s %d",order[i].pid,order[i].wt);
+        printf(" %s %d",p[i].pid,p[i].wt);
 
     printf("\nTurnaround Time:");
     for(i=0;i<n;i++)
-        printf(" %s %d",order[i].pid,order[i].tat);
+        printf(" %s %d",p[i].pid,p[i].tat);
 
     printf("\nAverage Waiting Time: %.2f",avg_wt/n);
     printf("\nAverage Turnaround Time: %.2f",avg_tat/n);
