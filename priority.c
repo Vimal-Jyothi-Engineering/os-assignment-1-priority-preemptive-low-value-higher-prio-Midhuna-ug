955c77f2c166b54ffa4b58a033183bbbf725aded
Midhuna K <midhunak633@gmail.com>
2026-03-11 02:47:39 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-struct process
-{
+struct process{
     char pid[10];
     int at, bt, pr;
     int ct, wt, tat;
@@ -10,45 +10,49 @@ struct process
 
 int main()
 {
-    int n, i, time = 0, completed = 0, idx;
-    float avg_wt = 0, avg_tat = 0;
+    int n,i,time=0,completed=0,idx;
+    float avg_wt=0,avg_tat=0;
+
     struct process p[20];
+    struct process order[20];
 
-    scanf("%d", &n);
+    scanf("%d",&n);
 
-    for(i = 0; i < n; i++)
+    for(i=0;i<n;i++)
     {
-        scanf("%s %d %d %d", p[i].pid, &p[i].at, &p[i].bt, &p[i].pr);
-        p[i].done = 0;
+        scanf("%s %d %d %d",p[i].pid,&p[i].at,&p[i].bt,&p[i].pr);
+        p[i].done=0;
     }
 
-    while(completed < n)
+    while(completed<n)
     {
-        idx = -1;
+        idx=-1;
 
-        for(i = 0; i < n; i++)
+        for(i=0;i<n;i++)
         {
-            if(p[i].done == 0 && p[i].at <= time)
+            if(p[i].done==0 && p[i].at<=time)
             {
-                if(idx == -1 || p[i].pr < p[idx].pr || 
-                  (p[i].pr == p[idx].pr && p[i].at < p[idx].at))
+                if(idx==-1 || p[i].pr<p[idx].pr || 
+                   (p[i].pr==p[idx].pr && p[i].at<p[idx].at))
                 {
-                    idx = i;
+                    idx=i;
                 }
             }
         }
 
-        if(idx != -1)
+        if(idx!=-1)
         {
-            time += p[idx].bt;
-            p[idx].ct = time;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
+            time+=p[idx].bt;
+            p[idx].ct=time;
+            p[idx].tat=p[idx].ct-p[idx].at;
+            p[idx].wt=p[idx].tat-p[idx].bt;
 
-            avg_wt += p[idx].wt;
-            avg_tat += p[idx].tat;
+            order[completed]=p[idx];
 
-            p[idx].done = 1;
+            avg_wt+=p[idx].wt;
+            avg_tat+=p[idx].tat;
+
+            p[idx].done=1;
             completed++;
         }
         else
@@ -58,15 +62,15 @@ int main()
     }
 
     printf("Waiting Time:");
-    for(i = 0; i < n; i++)
-        printf(" %s %d", p[i].pid, p[i].wt);
+    for(i=0;i<n;i++)
+        printf(" %s %d",order[i].pid,order[i].wt);
 
     printf("\nTurnaround Time:");
-    for(i = 0; i < n; i++)
-        printf(" %s %d", p[i].pid, p[i].tat);
+    for(i=0;i<n;i++)
+        printf(" %s %d",order[i].pid,order[i].tat);
 
-    printf("\nAverage Waiting Time: %.2f", avg_wt/n);
-    printf("\nAverage Turnaround Time: %.2f", avg_tat/n);
+    printf("\nAverage Waiting Time: %.2f",avg_wt/n);
+    printf("\nAverage Turnaround Time: %.2f",avg_tat/n);
 
     return 0;
 }
